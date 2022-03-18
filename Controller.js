d1d6dbc80ed0abe5e2c01629b0b0d1708241f7d2
[DenisVoropaev] <de.voro@mail.ru>
2022-03-18 15:08:36 +0300

    Release 1

--- Controller.js
+++ Controller.js
@@ -1,24 +1,58 @@
+const readline = require('readline');
+const {stdin: input, stdout: output} = require('process');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const Model = require("./Model");
+const View = require("./View");
+const {Model, Question} = require("./View");
+
+let n = 0;
+let length = 0;
+
 class Controller {
-  constructor(model, view) {
-    this.model = model
-    this.view = view
+  #model;
+  #view;
+
+  constructor() {
+    this.#model = new Model();
+    this.#view = new View();
+    
   }
 
   run() {
-    // Просим экземпляр класса модели прочитать папку со всеми темами и составить меню.
-    // Попутно передаем метод контроллера this.printTopicsController,
-    // так как нам нужно отправить сформинованное меню на вывод в экземпляр класса view
-    // после того, как завершится асинхронная операция чтения папки
-    // Здесь this.printTopicsController — является callback'ом  
-    this.model.readTopics(this.printTopicsController)
+    const txt1 = this.#view.writeStartQuestions(this.#model.getNameOfTopic());
+    rl.question(txt1, (answer) => {
+      const arrBuf = txt1.splice('\n');
+      length = arrBuf.length;
+      this.print();
+    });
   }
 
-  printTopicsController(topicsMenu) {
+  print() {
     // Тут нужно попросить экземпляр класса view вывести меню пользователю, 
     // а также дождаться ответа последнего
+    if (n < length) {
+    const currentObject = this.#model.readFile(n);
+    const show = this.#view.writeQuestions(currentObject.question);
+    rl.question(show, (answer) => {
+      if (answer === currentObject.answer) {
+        this.#view.writeGood();
+        n+=1;
+        this.print();
+      }  else {
+        this.#view.writeWrong();
+        const score = (n / length) * 100;
+        this.#view.writeScore(score);
+        rl.close();
+      }
+      
+    });
+    }
   }
 
   
 }
 
-module.exports = Controller
+module.exports = Controller;
